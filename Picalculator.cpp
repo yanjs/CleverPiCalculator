79a47e7085b60e862f9386acbf901d4d7a7dece9
yanjs <felixyjs@gmail.com>
2020-04-15 00:43:40 -0400

    Makes it a real cpp program

--- Picalculator.cpp
+++ Picalculator.cpp
@@ -1,61 +1,52 @@
-//Settings:
-//Use macro functions?
-#define USE_MACRO_FUNCTIONS
-#define SHOW_ARCCOS_V2_
-#define POWER 16
-#define BASE 10
-
-//Main:
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-#define LD long double
-#define M1 1
-#define M2 pow(BASE,POWER)
-#ifdef USE_MACRO_FUNCTIONS
-	#define GetV1(X,Y) (T11*(X)+T12*(Y))
-	#define GetV2(X,Y) (T21*(Y)+T22*(X))
-#else
-	LD T11, T12, T21, T22;
-	LD GetV1(LD &, LD &);
-	LD GetV2(LD &, LD &);
-	inline LD GetV1(LD &v1, LD &v2){
-		return T11*v1+T12*v2;
-	}
-	inline LD GetV2(LD &v1, LD &v2){
-		return T21*v2+T22*v1;
-	}
-#endif
-using namespace std;
-int main(){
-	LD v11 = 0, v21 = -1;
-	LD v12 = 0;//, v22 = 0;
-#ifdef USE_MACRO_FUNCTIONS
-	LD T11, T12, T21, T22;
-#endif
-	register long long count = 0;
-	{
-		LD sum = M1 + M2;
-		LD min1 = M1 - M2;
-		LD min2 = M2 - M1;
-		LD M1M1 = 2 * M1;
-		LD M2M2 = 2 * M2;
-		T11 = min1 / sum;
-		T12 = M2M2 / sum;
-		T21 = min2 / sum;
-		T22 = M1M1 / sum;
-	}
-	while(v11>v21){
-		count++;
-		v12 = GetV1(v11, v21);
-#ifdef SHOW_ARCCOS_V2
-		cout<<acos(v21)<<endl;
-#endif
-		if(v12>=0)break;
-		v21 = GetV2(v11, v21);
-		count++;
-		v11 = -v12;
-	}
-	cout<<setbase(BASE)<<count<<" times\n";
-	return 0;
+typedef long double ld;
+constexpr bool SHOW_ARCCOS_V2 = false;
+
+using std::cout;
+using std::endl;
+using std::setbase;
+using std::acos;
+using std::pow;
+
+int main() {
+    // change *digit* and *base* to see different results!
+    // digit: number of sig-fig of pi
+    // base: can be 8, 10 or 16
+    constexpr auto digit = 8;
+    constexpr auto base = 16;
+
+    constexpr auto power = 2 * (digit - 1);
+    constexpr ld m1 = 1;
+    constexpr ld m2 = pow(base, power);
+
+    size_t count = 0;
+    // velocity to left < 0
+    ld v1 = 0, v2 = -1;
+    ld temp_v1;
+
+    // m1 will hit m2 iff v1 > v2;
+    // m1 will hit the wall iff v1 < 0;
+    while (v1 > v2) {
+        if constexpr (SHOW_ARCCOS_V2) {
+            cout << acos(v2) << endl;
+        }
+        // m1 hits m2
+        count++;
+        // temp_v1 = ((m2 - m1) * v2 + 2 * m1 * v1) / (m1 + m2);
+        temp_v1 = ((m1 - m2) / (m1 + m2)) * v1 + (2 * m2 / (m1 + m2)) * v2;
+
+        // m1 goes right after she hits m2
+        if (temp_v1 >= 0) break;
+
+        // v2 = ((m1 - m2) * v1 + 2 * m2 * v2) / (m2 + m1);
+        v2 = ((m2 - m1) / (m1 + m2)) * v2 + (2 * m1 / (m1 + m2)) * v1;
+        // m1 hits the wall and changes the direction of velocity
+        v1 = -temp_v1;
+        count++;
+    }
+    cout << "The collision happens" << setbase(base) << count << " times" << endl;
+
+    return 0;
 }
